9ab9624f10f63764ee989f2551023f74028419d9
DavLopezL <142951736+DavLopezL@users.noreply.github.com>
2024-08-06 15:47:09 -0500

    Trabajo 3

---
+++ Ejercicio22Capitulo4/Ejercicio22Capitulo4.java
@@ -0,0 +1,9 @@
+package Ejercicio22Capitulo4;
+
+public class Ejercicio22Capitulo4 {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.show(true);
+    }
+}
---
+++ Ejercicio22Capitulo4/Empleado.java
@@ -0,0 +1,12 @@
+package Ejercicio22Capitulo4;
+
+public class Empleado {
+    String nombre;
+    double SalarioHora, HorasTrabajadas; //Atributos de la clase Empleado
+    
+    Empleado(String nombre, double SalarioHora, double HorasTrabajadas) {
+        this.nombre = nombre;
+        this.SalarioHora = SalarioHora;
+        this.HorasTrabajadas = HorasTrabajadas;
+        } //Constructor del empleado
+}
