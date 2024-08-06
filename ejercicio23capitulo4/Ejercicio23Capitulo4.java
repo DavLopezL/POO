b413a9b7a0a8dc528026b250d179de66475a1308
DavLopezL <142951736+DavLopezL@users.noreply.github.com>
2024-08-06 15:48:10 -0500

    Trabajo 3

---
+++ ejercicio23capitulo4/Ejercicio23Capitulo4.java
@@ -0,0 +1,9 @@
+package ejercicio23capitulo4;
+
+public class Ejercicio23Capitulo4 {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.show(true);
+    }
+}
